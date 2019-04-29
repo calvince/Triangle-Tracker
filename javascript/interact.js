35d37ecc4ec6b37abe34c051aca4937204a8641f
calvince <calmosh1421@gmail.com>
2019-04-29 04:33:09 +0300

    added differect  functions and the arguments to confirm different type of triangle

--- javascript/interact.js
+++ javascript/interact.js
@@ -4,4 +4,19 @@ class mainTriangle{
         this.dimen2 = b;
         this.dimen3 = c;
     }
+    checkTriangle(){
+        return this.dimen1 <=0 || this.dimen2 <=0 || this.dimen3 <=0;
+    }
+    isNotTriangle(){
+        return this.dimen1 + this.dimen2 <= this.dimen3 || this.dimen1 + this.dimen3 <= this.dimen2 || this.dimen3 + this.dimen2 <= this.dimen1;
+    }
+    isEquilateral(){
+        return this.dimen1 === this.dimen2 && this.dimen2 === this.dimen3;
+    }
+    isIsosceles(){
+        return this.dimen1 === this.dimen2 || this.dimen2 === this.dimen3 || this.dimen1 === this.dimen3;
+    }
+    isScalene(){
+        return !this.isEquilateral();
+    }
 }

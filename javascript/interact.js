04f0c159ffd60645b2a4976b17191bf0230491dd
calvince <calmosh1421@gmail.com>
2019-04-29 06:35:38 +0300

    deleted the myMainFunction and replaced it with myFunction

--- javascript/interact.js
+++ javascript/interact.js
@@ -21,15 +21,27 @@ class mainTriangle{
     }
 }
 
-function myMainFunction(){
-    var sideA= parseFloat(document.getElementById('A').value);
-    var sideB= parseFloat(document.getElementById('B').value);
-    var sideC= parseFloat(document.getElementById('C').value);
+function myFunction(){
+    var sideA= parseFloat(document.getElementById('first').value);
+    var sideB= parseFloat(document.getElementById('second').value);
+    var sideC= parseFloat(document.getElementById('third').value);
     
     var newTriangle = new mainTriangle(sideA,sideB,sideC);
     // console.log(newTriangle);
     if(newTriangle.checkTriangle()){
         document.getElementById('tell').innerHTML = 'Cannot be a triangle.';
+    }
+    else if(newTriangle.isNotTriangle()){
+        document.getElementById('tell').innerHTML = 'NOT a triangle.'
+    }
+    else if(newTriangle.isEquilateral()){
+        document.getElementById('tell').innerHTML = 'Equilateral triangle.'
+    }
+    else if(newTriangle.isIsosceles()){
+        document.getElementById('tell').innerHTML = 'Iscosceles triangle.'
+    }
+    else{
+        document.getElementById('tell').innerHTML = 'Scalene triangle.'
     }
     
     
